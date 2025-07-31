d05771bacd16e96a475448106b3d32a746ad26ab
Abdulrahman Awwad <111631238+AbdoAwwad93@users.noreply.github.com>
2025-07-31 19:52:38 +0300

    [706] [Time Beats: 8.00%] [Memory Beats: 99.17%] - LeetPush

---
+++ Array Hashing/Design-HashMap.cpp
@@ -0,0 +1,42 @@
+class MyHashMap {
+public:
+    vector<pair<int,int>> map;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        for(int i = 0;i<map.size();i++){
+            if(map[i].first==key){
+                map[i].second = value;
+                return;
+            }
+        }
+       map.push_back(make_pair(key,value));
+    }
+    
+    int get(int key) {
+        for(int i = 0;i<map.size();i++){
+            if(map[i].first==key){
+                return map[i].second;
+            }
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        for(int i = 0;i<map.size();i++){
+            if(map[i].first==key){
+                map.erase(map.begin()+i);
+            }
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
